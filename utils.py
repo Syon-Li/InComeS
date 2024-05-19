7e64ff1dc2632ff4c2af8fca0b67ba975d7022bc
LI SHUAIYI <li_shuaiyi2021@outlook.com>
2024-05-19 12:39:03 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,27 +2,6 @@ import torch
 import math
 import numpy as np
 
-# class MyIterableDataset(torch.utils.data.IterableDataset):
-#     def __init__(self, start, end):
-#         super(MyIterableDataset).__init__()
-#         assert end > start, "this example code only works with end >= start"
-#         self.start = start
-#         self.end = end
-#     def __iter__(self):
-#         worker_info = torch.utils.data.get_worker_info()
-#         if worker_info is None:  # single-process data loading, return the full iterator
-#             iter_start = self.start
-#             iter_end = self.end
-#         else:  # in a worker process
-#             # split workload
-#             per_worker = int(math.ceil((self.end - self.start) / float(worker_info.num_workers)))
-#             worker_id = worker_info.id
-#             iter_start = self.start + worker_id * per_worker
-#             iter_end = min(iter_start + per_worker, self.end)
-#         return iter(range(iter_start, iter_end))
-    
-
-
 
 class MyIterableDataset(torch.utils.data.IterableDataset):
     def __init__(self, mem_arr, gist_activations, gist_location_id, config):
