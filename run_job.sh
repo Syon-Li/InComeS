179348d56fe72234b8881ef1c916f0979a707fd1
LI SHUAIYI <li_shuaiyi2021@outlook.com>
2024-05-19 13:03:37 +0800

    Delete run_job.sh

--- run_job.sh
+++
@@ -1,6 +0,0 @@
-echo clear 0 1 2 3>/jizhi/jizhi2/worker/trainer/.cfile
-python -m accelerate.commands.launch --num_processes=1 --mixed_precision=fp16 --gpu_ids=[0,1,2,3] /apdcephfs_qy3/share_733425/zhisonzhang/users/shuaiyili/edit_train.py
-echo >/jizhi/jizhi2/worker/trainer/.cfile
-
-
-# python /apdcephfs_qy3/share_733425/zhisonzhang/users/shuaiyili/edit_train.py
