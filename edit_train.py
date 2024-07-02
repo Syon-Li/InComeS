7f62ae7137cb2ab3d8415ff772728a362997622d
LI SHUAIYI <li_shuaiyi2021@outlook.com>
2024-07-02 14:54:31 +0800

    Update edit_train.py

--- edit_train.py
+++ edit_train.py
@@ -6,36 +6,9 @@ import argparse
 import torch
 import pickle
 import json
-import logging
 import os
-from datetime import datetime
 
-
-# now = datetime.now()
-# formatted_time = now.strftime('%Y-%m-%d_%H-%M-%S')
-
-# logger = logging.getLogger(__file__)
-# log_file = "log_{}".format(formatted_time)
-# log_path = "logs/{}".format(log_file)
-
-# fileHandler = logging.FileHandler(log_path, mode = 'w')
-# fileHandler.setLevel(logging.DEBUG)
-# consoleHandler = logging.StreamHandler()
-# consoleHandler.setLevel(logging.DEBUG)
-
-# # set formatter
-# formatter = logging.Formatter('[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-# consoleHandler.setFormatter(formatter)
-# fileHandler.setFormatter(formatter)
-
-# # add
-# logger.addHandler(fileHandler)
-# logger.addHandler(consoleHandler)
-
-# logger.info("test")
-
-
-
+os.environ["NCCL_DEBUG"] = "INFO"
 
 
 def main():
@@ -44,36 +17,37 @@ def main():
     parser.add_argument('--gradaccu', type=int, default=1, help='number of gradient steps to use')
     parser.add_argument('--batch_size', type=int, nargs="+", default=[8,8,8,8], help='the batch size for each dataloader')
     parser.add_argument("--gist_num", type=int, default=10, help="number of gist token activations to keep")
-    parser.add_argument('--num_gpu', type=int, default=1, help='number of gpus to use')
     args = parser.parse_args()
+
+    end_point = 1e8
 
     lr = 4e-5
     min_lr = 4e-6
 
-    warmup_updates = 1000
+    warmup_updates = 2000
     max_updates = 20000
     save_updates = 2000
 
-    warmup_updates_per_gpu = warmup_updates / args.num_gpu
-    max_updates_per_gpu = max_updates / args.num_gpu
-    save_updates_per_gpu = save_updates / args.num_gpu
+    warmup_iters = int(warmup_updates * args.gradaccu)
+    max_iters = int(max_updates * args.gradaccu)
+    save_iters = int(save_updates * args.gradaccu)
 
-    warmup_iters = int(warmup_updates_per_gpu * args.gradaccu)
-    max_iters = int(max_updates_per_gpu * args.gradaccu)
-    save_iters = int(save_updates_per_gpu * args.gradaccu)
-
-    loss_f_name = "training_loss"
+    loss_f_name = "training_loss_TinyLlama_Chat"
     if os.path.exists("{}.json".format(loss_f_name)):
         loss_f_name = loss_f_name + "_new"
     
     checkpoint_dir = "./checkpoints/slimpajama_10B"
+    model_name = "TinyLlama-1.1B-Chat"
 
     
 
 
 
-    model = Edit_LlamaForCausalLM.from_pretrained("/apdcephfs_qy3/share_733425/zhisonzhang/zh/2401mygo/_cache/models--TinyLlama--TinyLlama-1.1B-intermediate-step-1431k-3T/snapshots/036fa4651240b9a1487f709833b9e4b96b4c1574")
-    tokenizer = AutoTokenizer.from_pretrained("/apdcephfs_qy3/share_733425/zhisonzhang/zh/2401mygo/_cache/models--TinyLlama--TinyLlama-1.1B-intermediate-step-1431k-3T/snapshots/036fa4651240b9a1487f709833b9e4b96b4c1574")
+    # model = Edit_LlamaForCausalLM.from_pretrained("/apdcephfs_qy3/share_733425/zhisonzhang/zh/2401mygo/_cache/models--TinyLlama--TinyLlama-1.1B-intermediate-step-1431k-3T/snapshots/036fa4651240b9a1487f709833b9e4b96b4c1574")
+    # tokenizer = AutoTokenizer.from_pretrained("/apdcephfs_qy3/share_733425/zhisonzhang/zh/2401mygo/_cache/models--TinyLlama--TinyLlama-1.1B-intermediate-step-1431k-3T/snapshots/036fa4651240b9a1487f709833b9e4b96b4c1574")
+
+    model = Edit_LlamaForCausalLM.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0", cache_dir="transformers_cache")
+    tokenizer = AutoTokenizer.from_pretrained("TinyLlama/TinyLlama-1.1B-Chat-v1.0", cache_dir="transformers_cache")
     # print(model)
 
 
@@ -108,7 +82,7 @@ def main():
         # print("num_lines", num_lines)
         f_path_set.append(f_path)
         start_set.append(0)
-        end_set.append(2048)
+        end_set.append(int(end_point))
 
     ds = FileIterD(f_path_set=f_path_set, start_set=start_set, end_set=end_set, batch_size=args.batch_size)
     train_dataloader = torch.utils.data.DataLoader(ds, collate_fn=collate_fn_fileD)
@@ -118,6 +92,7 @@ def main():
     accelerator = Accelerator(
                             gradient_accumulation_steps=args.gradaccu, 
                             dataloader_config=DataLoaderConfiguration(dispatch_batches=False), 
+                            log_with="wandb",
                             )
     print(accelerator.device, accelerator.process_index, accelerator.distributed_type,)
     # model = model.to(device)
@@ -129,6 +104,9 @@ def main():
 
     # accelerator.load_state(input_dir="/apdcephfs_qy3/share_733425/zhisonzhang/users/shuaiyili/Gist_slimpajama_checkpoint")
     # print(accelerator.state)
+    config = {"model": model_name,
+              "dataset": "slimpajama_10B",}
+    accelerator.init_trackers("gist-model-editing", config=config)
     model.train()
     for local_step, (global_step, input_ids, attention_mask, labels) in enumerate(train_dataloader):
         with accelerator.accumulate(model):
@@ -142,7 +120,6 @@ def main():
                     gist_pool_idx[i,col] = 1
                     col += 1
             
-            print(input_ids, input_ids.shape, local_step+1, global_step+1)
             # input_ids = input_ids.to(device)
             # attention_mask = attention_mask.to(device)
             outputs, gist_pool, loss_set = model(input_ids=input_ids, 
@@ -158,13 +135,17 @@ def main():
             # print(loss_set)
             accelerator.backward(loss)
             optimizer.step()
-            lr_updated = get_lr(global_step+1, warmup_iters=warmup_iters, lr_decay_iters=max_iters, min_lr=min_lr, learning_rate=lr)
+            lr_updated = get_lr(local_step+1, warmup_iters=warmup_iters, lr_decay_iters=max_iters, min_lr=min_lr, learning_rate=lr)
             for param_group in optimizer.param_groups:
                 param_group["lr"] = lr_updated
                 # print("it;lr",it, param_group["lr"])
             optimizer.zero_grad()
 
-        loss_set.update({"global_step":global_step+1, "local_step":local_step+1})
+            loss_set.update({"lr":lr_updated})
+            accelerator.log(loss_set, step=global_step+1)
+            # print('{}, local_step:{}, global_step:{}, lr:{}'.format(input_ids.shape, local_step+1, global_step+1, lr_updated))
+
+        loss_set.update({"input_ids_shape":input_ids.shape, "lr":lr_updated, "global_step":global_step+1, "local_step":local_step+1})
         with open("{}.json".format(loss_f_name), "a") as file:
             json.dump(loss_set, file)
             file.write("\n")
@@ -184,12 +165,10 @@ def main():
                 checkpoint_config = {"gist_pool":gist_pool, "global_step":global_step+1, "local_step":local_step+1}
                 with open('{}/checkpoint_{}/checkpoint_config.pickle'.format(checkpoint_dir, local_step+1), 'wb') as f:
                     pickle.dump(checkpoint_config, f)
-
-     
-
+    
     accelerator.wait_for_everyone()
-    unwrapped_model = accelerator.unwrap_model(model)
-    unwrapped_model.save_pretrained("{}/model".format(checkpoint_dir), is_main_process=accelerator.is_main_process, save_function=accelerator.save,)
+    accelerator.save_model(model, "{}/model".format(checkpoint_dir))
+    accelerator.end_training()
             
 
 
